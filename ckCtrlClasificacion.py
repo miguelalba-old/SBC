01a4e1455857cdea17f4d228c11cf7b9ac8cc9da
miguelalba <miguelalba@users.noreply.github.com>
2014-12-14 10:21:07 +0100

    controlador limpito :)

--- ckCtrlClasificacion.py
+++ ckCtrlClasificacion.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
-"""
-Created on Sun Jan 19 10:28:45 2014
 
-@author: acalvo
-"""
 from PyQt4 import QtGui
-#from PyQt4 import QtCore
 
 import ckModApClasificacion as ma
 
 
 def eventClasificar(clasificacionDlg):
-    pass
-    print 'Objeto:', clasificacionDlg.objeto
-    print '================================\n'
-    mp=ma.MetodoPoda(clasificacionDlg.objeto) #Se crea la instancia del método de la poda
-    r,exp=mp.execute()#se ejecuta el método
-    clasificacionDlg.results_widget.clear()#Se borra la explicación
-    clasificacionDlg.results_widget.appendPlainText(exp)#Se presenta la nueva explicación
+    "Classification event"
+    pruning_method = ma.MetodoPoda(clasificacionDlg.objeto)
+    candidate_classes, reason = pruning_method.execute()
+
+    # Update results_widget
+    clasificacionDlg.results_widget.clear()
+    clasificacionDlg.results_widget.appendPlainText(reason)
     clasificacionDlg.results_widget.moveCursor(QtGui.QTextCursor.Start)
-    cs=[]
-    for cc in r:
-        print '         ->',cc.nombre
-        cs.append(cc.nombre)
+
+    # Update selected_class_widget
+    selected_classes = [candidate_class.nombre
+                        for candidate_class in candidate_classes]
     clasificacionDlg.selected_class_widget.clear()
-    clasificacionDlg.selected_class_widget.addItems(cs) #Se añaden las clases resultado de la clasificación
-                                                                # al control listbox para que lo presente.
-    return
-
-
-
+    clasificacionDlg.selected_class_widget.addItems(selected_classes)
